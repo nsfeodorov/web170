b893cae9e57d72e9c97d17349bfda594432b0fe8
Nicole Feodorov <nsfeodorov@gmail.com>
2017-05-16 14:46:53 -0700

    Assignment 6: Create Your Main & Sub Menu Systems
    Still looks terrible.  Problems with my CSS and WP's classes?  :-[

--- index.php
+++ index.php
@@ -59,14 +59,14 @@
 			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __(''))); // list the sub-pages
 			} else { // if the page does not have a parent-
 	
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => __(''))); // list the sub-pages with no title
+			wp_list_pages(array('child_of' => $post->ID, 'title_li' => __(''))); // list the sub-pages (no title)
 			} ?></ul>
 
 
 <?php if (!(is_page())) : // if not in "pages" (in post) ?>
 
 <h3 class="sub-navigation-title">Blog</h3>
-<ul class="sub-navigation-items"><?php wp_list_categories(array('title_li' => __(''))); // ...list the categories with no title ?></ul>
+<ul class="sub-navigation-items"><?php wp_list_categories(array('title_li' => __(''))); // list the categories (no title) ?></ul>
 
 <?php endif; ?>
 
