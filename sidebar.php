01fcb2390ee2014c105764d2763da1ff6e734868
Nicole Feodorov <nsfeodorov@gmail.com>
2017-05-27 09:02:00 -0700

    Assignment 10: Create a pull-quote for your site
    Assignment 10: Create a pull-quote for your site using custom fields

--- sidebar.php
+++ sidebar.php
@@ -1,24 +1,54 @@
 
 
 <div id="sidebar">
+     
+     <!--sub navigation-->
       
-		  <h3 class="sub-navigation-title"><?php echo get_the_title($post->post_parent); // get the gateway page title ?></h3>
-
-			<ul class="sub-navigation-items"><?php if ($post->post_parent) { // if the page has parent-
+	<div id="sub-navigation">
 	
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __(''))); // list the sub-pages
-			} else { // if the page does not have a parent-
+	<?php if (is_page()) : // if in pages- ?>
+    <h2 class="sub-navigation-title"><?php echo get_the_title($post->post_parent); // get gateway page title ?></h2>
+    
+    <ul class="sub-navigation-items"><?php 
+    
+    if ($post->post_parent) { // if page has a parent-
+        
+        wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __(''))); // list parent
+        
+    } else { // if page does not have a parent-
+    
+        wp_list_pages(array('child_of' => $post->ID, 'title_li' => __(''))); // list child
+     
+    }
+    
+    ?></ul>
+    
+    <?php endif; ?>
+    
+    
+    <?php if (!(is_page())) : // if not in pages- ?>
+    
+    <h2 class="sub-navigation-title">Blog</h2>
+    
+    <ul class="sub-navigation-items"><?php wp_list_categories(array('title_li' => __(''))); // list categories ?></ul>
+    
+    <?php endif; ?>
+    
+    </div> <!--end of sub-navigation-->
 	
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => __(''))); // list the sub-pages (no title)
-			} ?></ul>
+	
+	<!-- Quote -->
+  <div id="quote">
+   
+    <?php if (get_post_meta($post->ID, 'Quote', true)) : // if there is a quote go get it ?>
+    
+    <blockquote class="sidebar">"<?php echo get_post_meta($post->ID, 'Quote', true); // show the quote ?>"</blockquote>
+    
+    <?php endif; ?> 
+    
+	</div><!--end of quote loop-->
+	
+	
+	
 
-
-<?php if (!(is_page())) : // if not in "pages" (in post) ?>
-
-<h3 class="sub-navigation-title">Blog</h3>
-<ul class="sub-navigation-items"><?php wp_list_categories(array('title_li' => __(''))); // list the categories (no title) ?></ul>
-
-<?php endif; ?>
-
-
-	</div><!--sidebar-->
+	</div><!--end sidebar-->
