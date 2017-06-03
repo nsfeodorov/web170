f62b74edc3c7525da5a6254f972a71f7ef92819a
Nicole Feodorov <nsfeodorov@gmail.com>
2017-06-03 16:23:18 -0700

    Assignment 12) Search Engine Optimize Your Site

--- functions.php
+++ functions.php
@@ -27,8 +27,38 @@ register_nav_menus(array(
 
 //create post thumbnails
 add_theme_support( 'post-thumbnails' );
-//
 
 
+add_post_type_support( 'page', 'excerpt' );// enable excerpts for pages
+
+
+//use Mike's 'get the title tag code' 
+
+function get_my_title_tag() {
+	
+	global $post;  //be sure to include this
+	
+	if (is_front_page()) { //this if the front page
+	bloginfo('description'); //retrieve the site's tagline
+	
+	} elseif (is_page() || is_single ()) { //this is for the site's pages or postings
+	
+	the_title(); //retrieve the page of post title
+	
+	} else { 
+	 bloginfo('description'); //retireive the site tagline
+	 }
+	
+	if ( $post->post_parent ) { // for your site's parent pages
+	
+		echo ' | '; //do this for the separator and include spaces
+		echo get_the_title($post->post_parent); //retrieve the parent page title
+	}
+	
+	echo ' | ';
+	bloginfo('name'); //retrieve the site name
+	echo ' | ';
+	echo 'Seattle, WA'; //write in the location
+}
 
 ?>
--- header.php
+++ header.php
@@ -5,8 +5,12 @@
   
   <meta charset="UTF-8">
    
-  <title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?>
- | Seattle, WA</title>
+   <!--Mike's title tag code-->
+  <title><?php get_my_title_tag(); ?></title>
+  
+  <!--put in Mike's meta code to enable excerpts for pages-->
+  <meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>" />
+   
    
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
@@ -15,6 +19,9 @@
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
   
 <!--put FontAwsome back in once I figure out the problem-->
+ 
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
  
 <!--WordPress head-->
       <?php wp_head(); ?>
@@ -27,6 +34,8 @@
  <div id="header">
     
  <h1 class="john" style="display: inline-block"> <a href="http://www.nicolefeodorov.com/scc/web170/wordpress">John Feodorov</a></h1>
+ 
+ <img id="toggle" src="php bloginfo('template_directory'); ?>/images/toggle-icon.png" width="25" height="25" alt="Toggle Menu">
  
  <!--begin navigation--> 
  <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => 'div', 'container_id' => 'navigation', ) ); ?>
